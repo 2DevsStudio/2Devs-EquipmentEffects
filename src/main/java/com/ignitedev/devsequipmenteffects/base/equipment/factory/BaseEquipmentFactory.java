0e51281a529a53106f0d5592d067f7ebdfb58ba6
ignitedev <ignitedevcontact@gmail.com>
2021-03-26 03:39:18 +0100

    Factory Converting ItemStack to BaseEquipment

--- src/main/java/com/ignitedev/devsequipmenteffects/base/equipment/factory/BaseEquipmentFactory.java
+++ src/main/java/com/ignitedev/devsequipmenteffects/base/equipment/factory/BaseEquipmentFactory.java
@@ -3,9 +3,16 @@ package com.ignitedev.devsequipmenteffects.base.equipment.factory;
 import com.ignitedev.devsequipmenteffects.base.equipment.BaseEquipment;
 import com.ignitedev.devsequipmenteffects.factory.Factory;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public interface BaseEquipmentFactory extends Factory {
     
     ItemStack convertToItemStack(BaseEquipment baseEquipment);
+    
+    @Nullable BaseEquipment convertToBaseEquipment(ItemStack itemStack);
+    
+    List<BaseEquipment> convertToBaseEquipments(List<ItemStack> itemStacks);
     
 }
--- src/main/java/com/ignitedev/devsequipmenteffects/base/equipment/factory/DefaultBaseEquipmentFactory.java
+++ src/main/java/com/ignitedev/devsequipmenteffects/base/equipment/factory/DefaultBaseEquipmentFactory.java
@@ -1,17 +1,44 @@
 package com.ignitedev.devsequipmenteffects.base.equipment.factory;
 
 import com.ignitedev.devsequipmenteffects.base.equipment.BaseEquipment;
+import com.ignitedev.devsequipmenteffects.base.equipment.repository.BaseEquipmentRepository;
+import lombok.RequiredArgsConstructor;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
  * If you thinking for what is that wrapping, this wrapping is for future
  * expand to be sure that i'm able for example to add support of nbt holding ;)
  */
+@RequiredArgsConstructor
 public class DefaultBaseEquipmentFactory implements BaseEquipmentFactory {
+    
+    private final BaseEquipmentRepository baseEquipmentRepository;
     
     @Override
     public ItemStack convertToItemStack(BaseEquipment baseEquipment) {
         
         return baseEquipment.getItemStack();
+    }
+    
+    @Override
+    public @Nullable BaseEquipment convertToBaseEquipment(ItemStack itemStack) {
+        
+        return baseEquipmentRepository.findByItemStack(itemStack);
+    }
+    
+    @Override
+    public List<BaseEquipment> convertToBaseEquipments(List<ItemStack> itemStacks) {
+        
+        List<BaseEquipment> baseEquipments = new ArrayList<>();
+        
+        for (ItemStack itemStack : itemStacks) {
+            baseEquipments.add(convertToBaseEquipment(itemStack));
+        }
+        
+        return baseEquipments;
     }
 }
