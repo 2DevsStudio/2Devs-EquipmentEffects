3b60f38b579e637e043cbef3e6237b8c54d1a0e4
ignitedev <ignitedevcontact@gmail.com>
2021-08-16 11:55:32 +0200

    Minecraft Version  checker to support 1.8 - 1.17

---
+++ src/main/java/com/ignitedev/devsequipmenteffects/util/MinecraftVersion.java
@@ -0,0 +1,43 @@
+package com.ignitedev.devsequipmenteffects.util;
+
+import lombok.experimental.UtilityClass;
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
+
+@UtilityClass
+public class MinecraftVersion {
+
+  private final static String stringVersion;
+  private final static int intVersion;
+
+  static {
+    String fullVersion = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+    stringVersion = StringUtils.replace(fullVersion, "_", " ");
+    intVersion = Integer.parseInt(
+        StringUtils.replace(fullVersion.split("_")[1].split("_R")[0], "v", ""));
+  }
+
+  public static boolean is(int version2) {
+    return intVersion == version2;
+  }
+
+  public static boolean isAfter(int version2) {
+    return intVersion > version2;
+  }
+
+  public static boolean isBefore(int version2) {
+    return intVersion < version2;
+  }
+
+  public static boolean isOrAfter(int version2) {
+    return intVersion == version2 || intVersion > version2;
+  }
+
+  public static String getStringVersion() {
+    return stringVersion;
+  }
+
+  public static int getIntVersion() {
+    return intVersion;
+  }
+}
