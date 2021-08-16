1c547e2c99a8ae9d45940f400e3fdc2dff657ef0
ignitedev <ignitedevcontact@gmail.com>
2021-08-16 11:25:37 +0200

    Check system and implementation

---
+++ src/main/java/com/ignitedev/devsequipmenteffects/base/effect/BaseChecks.java
@@ -0,0 +1,13 @@
+package com.ignitedev.devsequipmenteffects.base.effect;
+
+import lombok.Data;
+
+@Data
+public class BaseChecks {
+
+  private final boolean displayNameCheck;
+  private final boolean loreCheck;
+  private final boolean enchantCheck;
+  private final boolean itemFlagCheck;
+
+}
--- src/main/java/com/ignitedev/devsequipmenteffects/base/equipment/BaseEquipment.java
+++ src/main/java/com/ignitedev/devsequipmenteffects/base/equipment/BaseEquipment.java
@@ -1,16 +1,19 @@
 package com.ignitedev.devsequipmenteffects.base.equipment;
 
 import com.ignitedev.devsequipmenteffects.EquipmentEffects;
+import com.ignitedev.devsequipmenteffects.base.effect.BaseChecks;
 import com.ignitedev.devsequipmenteffects.base.effect.BaseEffect;
 import com.ignitedev.devsequipmenteffects.base.effect.BaseTrigger;
 import com.ignitedev.devsequipmenteffects.base.player.BasePlayer;
 import com.ignitedev.devsequipmenteffects.interfaces.Applicable;
+import com.ignitedev.devsequipmenteffects.util.XMaterial;
 import java.util.List;
 import lombok.Data;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @Data
@@ -25,6 +28,7 @@ public class BaseEquipment implements Applicable {
 
   private final List<BaseEffect> effectList;
   private final BaseTrigger baseTrigger;
+  private final BaseChecks baseChecks;
 
   private final ItemStack itemStack;
 
@@ -39,19 +43,57 @@ public class BaseEquipment implements Applicable {
       if (targetItemStack == this.itemStack) {
         return true;
       } else {
-        Material comparisonType = this.itemStack.getType().isLegacy() ?
-            Bukkit.getUnsafe().fromLegacy(this.itemStack.getData(), true) :
-            this.itemStack.getType();
+        XMaterial itemstack = XMaterial.matchXMaterial(this.itemStack);
+        XMaterial targetItem = XMaterial.matchXMaterial(targetItemStack);
 
-        return comparisonType == targetItemStack.getType() &&
-            this.itemStack.hasItemMeta() == targetItemStack.hasItemMeta() &&
-            (!this.itemStack.hasItemMeta() ||
-                Bukkit.getItemFactory()
-                    .equals(this.itemStack.getItemMeta(), targetItemStack.getItemMeta())
-            );
+        if (itemstack == targetItem) {
+          return this.itemStack.hasItemMeta() == targetItemStack.hasItemMeta() && (
+              !this.itemStack.hasItemMeta() || Bukkit.getItemFactory()
+                  .equals(this.itemStack.getItemMeta(), targetItemStack.getItemMeta()));
+        }
       }
     }
+    return false;
   }
+
+  private boolean isSimilarWithoutNBTCheck(@NotNull ItemStack targetItemStack) {
+    XMaterial itemstack = XMaterial.matchXMaterial(this.itemStack);
+    XMaterial targetItem = XMaterial.matchXMaterial(targetItemStack);
+
+    if (itemstack != targetItem) {
+      return false;
+    }
+
+    ItemMeta targetMeta = targetItemStack.getItemMeta();
+    ItemMeta itemMeta = this.itemStack.getItemMeta();
+
+    if (baseChecks.isDisplayNameCheck()) {
+      if (targetMeta.hasDisplayName() && itemMeta.hasDisplayName()) {
+        if (!targetMeta.getDisplayName().equalsIgnoreCase(itemMeta.getDisplayName())) {
+          return false;
+        }
+      }
+    }
+    if (baseChecks.isLoreCheck()) {
+      if (targetMeta.hasLore() && itemMeta.hasLore()) {
+        if (!targetMeta.getLore().equals(itemMeta.getLore())) {
+          return false;
+        }
+      }
+    }
+    if (baseChecks.isEnchantCheck()) {
+      if (targetMeta.hasEnchants() && itemMeta.hasEnchants()) {
+        if (!targetMeta.getEnchants().equals(itemMeta.getEnchants())) {
+          return false;
+        }
+      }
+    }
+    if (baseChecks.isItemFlagCheck()) {
+      return targetMeta.getItemFlags().equals(itemMeta.getItemFlags());
+    }
+    return true;
+  }
+
 
   @Override
   public void apply(Player player) {
