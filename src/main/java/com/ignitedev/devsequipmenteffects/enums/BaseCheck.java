8e5fc6e05cda7baeb01f24255c3146d3926977de
ignitedev <ignitedevcontact@gmail.com>
2021-08-16 11:56:23 +0200

    BaseCheck enum supporting paths

--- src/main/java/com/ignitedev/devsequipmenteffects/enums/BaseCheck.java
+++ src/main/java/com/ignitedev/devsequipmenteffects/enums/BaseCheck.java
@@ -1,10 +1,17 @@
 package com.ignitedev.devsequipmenteffects.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum BaseCheck {
 
-  DISPLAY_NAME_CHECK,
-  LORE_CHECK,
-  ENCHANTMENT_CHECK,
-  ITEM_FLAG_CHECK;
+  DISPLAY_NAME_CHECK("item-checks.display-name-check"),
+  LORE_CHECK("item-checks.lore-check"),
+  ENCHANTMENT_CHECK("item-checks.enchantment-check"),
+  ITEM_FLAG_CHECK("item-checks.item-flag-check"),
+  ALL_CHECKS("item-checks.all-checks");
 
+  @Getter
+  private final String yamlPath;
 }
