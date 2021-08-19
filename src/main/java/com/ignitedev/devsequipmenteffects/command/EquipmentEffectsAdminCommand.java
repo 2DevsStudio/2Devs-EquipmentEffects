a8d6e688fe5e1e6f4075736dc9123b1382194422
ignitedev <ignitedevcontact@gmail.com>
2021-08-19 14:56:30 +0200

    Added /eea create command

--- src/main/java/com/ignitedev/devsequipmenteffects/command/EquipmentEffectsAdminCommand.java
+++ src/main/java/com/ignitedev/devsequipmenteffects/command/EquipmentEffectsAdminCommand.java
@@ -5,18 +5,30 @@ import com.ignitedev.devsequipmenteffects.base.equipment.BaseEquipment;
 import com.ignitedev.devsequipmenteffects.base.equipment.repository.BaseEquipmentRepository;
 import com.ignitedev.devsequipmenteffects.configuration.BaseConfiguration;
 import com.ignitedev.devsequipmenteffects.util.BaseUtil;
+import com.ignitedev.devsequipmenteffects.util.MinecraftVersion;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.logging.Level;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent.Builder;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.event.ClickEvent.Action;
 import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.TextColor;
+import org.apache.commons.io.IOUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 @RequiredArgsConstructor
@@ -26,6 +38,7 @@ public class EquipmentEffectsAdminCommand implements CommandExecutor {
   private final BaseEquipmentRepository baseEquipmentRepository;
   private final EquipmentEffects equipmentEffects;
 
+  @SneakyThrows
   @Override
   public boolean onCommand(@NotNull CommandSender sender,
       @NotNull Command command,
@@ -38,9 +51,14 @@ public class EquipmentEffectsAdminCommand implements CommandExecutor {
       return false;
     }
 
-    Player senderPlayer = (Player) sender;
-
     if (args.length == 2 && args[0].equalsIgnoreCase("give")) {
+      if (!(sender instanceof Player)) {
+        sender.sendMessage(BaseUtil.fixColor(baseConfiguration.getThisCommandIsPlayerOnly()));
+        return false;
+      }
+
+      Player senderPlayer = (Player) sender;
+
       String identifier = args[1];
       BaseEquipment baseEquipmentById = baseEquipmentRepository.findById(identifier);
 
@@ -80,18 +98,73 @@ public class EquipmentEffectsAdminCommand implements CommandExecutor {
         }
       }
       if (builder != null) {
+        if (!(sender instanceof Player)) {
+          sender.sendMessage(BaseUtil.fixColor(baseConfiguration.getThisCommandIsPlayerOnly()));
+          return false;
+        }
+        Player senderPlayer = (Player) sender;
+
         equipmentEffects.adventure.player(senderPlayer).sendMessage(builder.build());
         return true;
       }
       return true;
     } else if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
-      equipmentEffects.reloadConfig();
-      baseConfiguration.initialize(equipmentEffects.getConfig());
-      sender.sendMessage(BaseUtil.fixColor(baseConfiguration.getReloadMessage()));
+      reload(sender);
+      return true;
+    } else if (args.length == 2 && args[0].equalsIgnoreCase("create")) { // todo change args length
+      String itemID = args[1];
+
+      if (baseEquipmentRepository.findById(itemID) != null) {
+        sender.sendMessage(BaseUtil.fixColor(baseConfiguration.getItemAlreadyExists()));
+        return false;
+      }
+
+      if (!(sender instanceof Player)) {
+        sender.sendMessage(BaseUtil.fixColor(baseConfiguration.getThisCommandIsPlayerOnly()));
+        return false;
+      }
+      Player senderPlayer = (Player) sender;
+      ItemStack itemInHand;
+
+      if (MinecraftVersion.isBefore(9)) {
+        itemInHand = senderPlayer.getInventory().getItemInHand();
+      } else {
+        itemInHand = senderPlayer.getInventory().getItemInMainHand();
+      }
+
+      if (itemInHand.getType() == Material.AIR) {
+        senderPlayer.sendMessage(BaseUtil.fixColor(baseConfiguration.getNoItemInHand()));
+        return false;
+      }
+
+      InputStream resource = EquipmentEffects.INSTANCE.getResource("Items/default.yml");
+      File file = new File(baseConfiguration.getItemsDirectory().getPath(), itemID + ".yml");
+
+      if (file.createNewFile()) {
+        EquipmentEffects.INSTANCE.getLogger().log(Level.INFO, "Created new Item File!");
+      }
+      if (resource == null) {
+        throw new FileNotFoundException("Resource default.yml file not found!");
+      }
+      try (OutputStream outputStream = new FileOutputStream(file)) {
+        IOUtils.copy(resource, outputStream);
+      }
+      YamlConfiguration yamlConfiguration = YamlConfiguration.loadConfiguration(file);
+
+      yamlConfiguration.set("itemstack", itemInHand);
+      yamlConfiguration.set("id", itemID);
+      yamlConfiguration.save(file);
+
+      sender.sendMessage(BaseUtil.fixColor(baseConfiguration.getCreatedNewItem()));
       return true;
     }
-
     sender.sendMessage(BaseUtil.fixColor(baseConfiguration.getAdminCommandUsage()));
     return false;
+  }
+
+  private void reload(CommandSender commandSender) {
+    equipmentEffects.reloadConfig();
+    baseConfiguration.initialize(equipmentEffects.getConfig());
+    commandSender.sendMessage(BaseUtil.fixColor(baseConfiguration.getReloadMessage()));
   }
 }
